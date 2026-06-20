c8dac5e6fc5b1582d0c817f8d9b3ee60c9c2cfda
dotcomvaisala <nikitastender24@gmail.com>
2026-06-20 20:30:25 +0500

    Update logging configuration comment

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from google import genai
 from google.genai import errors
 from aiohttp import web
 
-# Настройка логирования
+# Настройка логирования1
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s")
 
 # ================= ФИНАЛЬНЫЕ КЛЮЧИ =================
